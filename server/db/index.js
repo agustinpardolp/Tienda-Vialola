cd284fc5279af63a6c3de7b2e835a59bb1c7ca87
Agustin Pardo <agustinpardo@MacBook-Pro-de-Agustin.local>
2020-10-12 11:09:03 -0300

    se agregaron cambios de compatibilidad en sequelize y psql

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,13 @@
 const Sequelize = require("sequelize")
 
 const db = new Sequelize("postgres://localhost:5432/vialola", {
-    logging: false
+    logging: false,
+    database: 'vialola',
+    username: 'postgres',
+    password: '1234',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres'
 })
 
 module.exports = db;
